58758579bb1598f191670a86b33da9a21e1dae53
eQip123 <eqip2019god@gmail.com>
2023-03-02 12:40:22 +0600

    added images and fonts

--- DeliveryApplication/ContentView.swift
+++ DeliveryApplication/ContentView.swift
@@ -9,14 +9,18 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
+        Image("food1")
             Text("Hello, World!")
+                .font(.custom("Poppins-Bold", size: 68))
+        }
+        init() {
+            for family in UIFont.familyNames.sorted() {
+                let names = UIFont.fontNames(forFamilyName: family)
+                print("family: \(family) Font names: \(names)")
+            }
         }
     }
-}
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
