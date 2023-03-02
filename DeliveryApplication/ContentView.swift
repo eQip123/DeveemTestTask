2b0ccfbd928574fda116ea0a944c9419a6204609
eQip123 <eqip2019god@gmail.com>
2023-03-02 11:46:54 +0600

    first commit

---
+++ DeliveryApplication/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  DeliveryApplication
+//
+//  Created by Aidar Asanakunov on 2/3/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, World!")
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ DeliveryApplication/DeliveryApplicationApp.swift
@@ -0,0 +1,17 @@
+//
+//  DeliveryApplicationApp.swift
+//  DeliveryApplication
+//
+//  Created by Aidar Asanakunov on 2/3/23.
+//
+
+import SwiftUI
+
+@main
+struct DeliveryApplicationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
